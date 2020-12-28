407d5454b57831e0338e03a2af6177e9a1381cf1
Jon-Thomas <jonel@samfundet.no>
2020-12-29 00:30:03 +0100

    Finished 10b

--- src/Day10/Day10.go
+++ src/Day10/Day10.go
@@ -23,23 +23,54 @@ func main() {
 
 	sort.Ints(numbers)
 
-	oneIntervals := 0
-	threeIntervals := 1 // Counting the Built-in adapter
+	// Algorithm:
+	// Number on each side of threeInterval can't be swapped out.
+	// All other numbers are marked as "Can be swapped out"
+	// 1C = 2		<= 1 Consecutive C's means two possible combinations
+	// 2C = 4		<= 2 Consecutive C's mean four possible combinations
+	// 3C = 7		<= 3 Consecutive C's mean seven possible combinations
+	// 4C = 11		<= 2 Consecutive C's mean eleven possible combinations
+	// nC =
+	// Total combinations is found by multiplying all possible combiations
+
+	thisSequenceOfOnes := 0
+	var sequenceList []int
 
 	for i := 1; i < len(numbers); i++ {
 
 		switch numbers[i] - numbers[i-1] {
 		case 1:
-			oneIntervals++
+			thisSequenceOfOnes++
 		case 3:
-			threeIntervals++
+			if thisSequenceOfOnes > 1 {
+				sequenceList = append(sequenceList, thisSequenceOfOnes-1)
+			}
+			thisSequenceOfOnes = 0
 		default:
 			fmt.Printf("Index=%d, number %d=%d; %d=%d\n", i, i-1, numbers[i-1], i, numbers[i])
 			return
 		}
 	}
 
-	fmt.Printf("%d*%d=%d\n", oneIntervals, threeIntervals, oneIntervals*threeIntervals)
+	if thisSequenceOfOnes > 1 {
+		sequenceList = append(sequenceList, thisSequenceOfOnes-1)
+	}
+
+	fmt.Println("Sequence: ", sequenceList)
+
+	sum := 1
+	for _, sequenceNr := range sequenceList {
+		switch sequenceNr {
+		case 1:
+			sum *= 2
+		case 2:
+			sum *= 4
+		case 3:
+			sum *= 7
+		}
+	}
+
+	fmt.Println("Sum: ", sum)
 	fmt.Println("END")
 }
 
