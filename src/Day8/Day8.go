4898749880ad3148d8234e1c15a1650fa7afe7c0
Jon-Thomas <jonel@samfundet.no>
2020-12-28 12:48:18 +0100

    Finished 8a

--- src/Day8/Day8.go
+++ src/Day8/Day8.go
@@ -3,33 +3,84 @@ package main
 import (
 	"fmt"
 	"jonthomas/AdventOfCode2020/files"
+	"strconv"
 )
 
-type fileLines struct {
-	Outer        string
-	Inner        string
-	HowManyInner int
+type instruction struct {
+	Operator string
+	Count    int
+	Visited  bool
 }
+
+// program is a key-value data structure, with pointers to instructions.
+// Pointers makes it possible to make changes to instructions
+type program map[int]*instruction
 
 func main() {
 
-	file, err := files.ReadFile("./Day8input.txt")
+	file, err := files.ReadFile("./Day8Input.txt")
 	if err != nil {
 		fmt.Println("File reading error", err)
 		return
 	}
 
-	parseErr := parseInput(file)
+	program, parseErr := parseInput(file)
 	if parseErr != nil {
 		fmt.Println("parsing error", err)
 		return
 	}
+
+	currentInstruction := program[0]
+	currentIndex := 0
+	accumulator := 0
+
+	for {
+		fmt.Printf("Current instruction: %d - %v\n", currentIndex, program[currentIndex])
+		if program[currentIndex].Visited {
+			break
+		}
+
+		program[currentIndex].Visited = true
+
+		switch currentInstruction.Operator {
+		case "nop":
+			currentIndex++
+			currentInstruction = program[currentIndex]
+			continue
+		case "acc":
+			currentIndex++
+			accumulator += currentInstruction.Count
+			currentInstruction = program[currentIndex]
+			continue
+		case "jmp":
+			currentIndex += currentInstruction.Count
+			currentInstruction = program[currentIndex]
+			continue
+		default:
+			fmt.Println("Unknown operator error: ", currentInstruction.Operator)
+			return
+		}
+	}
+
+	fmt.Println("Done. Accumulator = ", accumulator)
 }
 
-func parseInput(fileLines []string) error {
+func parseInput(fileLines []string) (program, error) {
 
-	for _, line := range fileLines {
-		fmt.Println(line)
+	var program = make(program)
+
+	for fileIndex, line := range fileLines {
+		instruction := instruction{}
+		instruction.Operator = line[0:3]
+		nr, nrErr := strconv.Atoi(line[4:])
+		if nrErr != nil {
+			return nil, nrErr
+		}
+		instruction.Count = nr
+		instruction.Visited = false
+		program[fileIndex] = &instruction
+
+		//fmt.Println(fileIndex, ": ", instruction, " - ", line)
 	}
-	return nil
+	return program, nil
 }
