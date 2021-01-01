8dc4f6f921bce77cc4e511252e5edaee5e3af4a9
Jon-Thomas <jonel@samfundet.no>
2021-01-02 00:40:54 +0100

    Finished 15b:
    New algorithm since method in 15a was very naïve took ages

--- src/Day15/Day15.go
+++ src/Day15/Day15.go
@@ -6,64 +6,68 @@ import (
 	"strings"
 )
 
+type numberUsage struct {
+	lastUsedRound      int
+	secondLastUsedDiff int
+	firstTime          bool
+}
+
+// key: The spoken number
+// value: Information on when the number was used
+type lastUsedIndex map[int]numberUsage
+
 func main() {
 
-	var gameHistory map[int]int
+	endAt := 30000000
 
-	gameHistory, parseErr := parseInput("1,17,0,10,18,11,6")
-	//gameHistory, parseErr := parseInput("3,1,2")
+	lastUsed, roundNr, lastNr, parseErr := parseInput("1,17,0,10,18,11,6")
+	//lastUsed, roundNr, lastNr, parseErr := parseInput("1,2,3")
 
 	if parseErr != nil {
 		fmt.Println("parsing error", parseErr)
 		return
 	}
 
-	//printParsedInput(gameHistory)
+	var numInfo numberUsage
+	exists := false
 
-	start := len(gameHistory)
-
-	for i := start; i < 2020; i++ {
-		numberToConsider := gameHistory[i-1]
-
-		alreadyUsed, numberOfTurnsAgo := checkGameHistory(i, numberToConsider, gameHistory)
-		if alreadyUsed {
-			gameHistory[i] = numberOfTurnsAgo
-			fmt.Printf("%d, ", numberOfTurnsAgo)
+	for i := roundNr; i <= endAt; i++ {
+		usageInfo, ok := lastUsed[lastNr]
+		if ok && usageInfo.firstTime {
+			// Insert 0, but first check if 0 has been used before
+			numInfo, exists = lastUsed[0]
+			lastUsed[0] = numberUsage{lastUsedRound: i, firstTime: !exists, secondLastUsedDiff: i - numInfo.lastUsedRound}
+			//fmt.Printf("%d: Considering %d. Inserting 0\n", i, lastNr)
+			lastNr = 0
 		} else {
-			gameHistory[i] = 0
-			fmt.Printf("0, ")
+			numberToInsert := usageInfo.secondLastUsedDiff
+			numInfo, exists = lastUsed[numberToInsert]
+			lastUsed[numberToInsert] = numberUsage{lastUsedRound: i, firstTime: !exists, secondLastUsedDiff: i - numInfo.lastUsedRound}
+			//fmt.Printf("%d: Considering %d. Inserting %d (%d was last used in round %d).\n", i, lastNr, numberToInsert, lastNr, usageInfo.lastUsedRound)
+			lastNr = numberToInsert
 		}
-
-	}
-
-	fmt.Printf("\nFound solution: %d \n", gameHistory[2019])
-}
-
-func checkGameHistory(currentRound int, numberToConsider int, gameHistory map[int]int) (bool, int) {
-	for i := currentRound - 2; i >= 0; i-- {
-		if gameHistory[i] == numberToConsider {
-			return true, currentRound - i - 1
+		if i%10000 == 0 {
+			fmt.Println("At round: ", i)
 		}
 	}
-	return false, 0
+
+	fmt.Printf("\nFound solution: %d \n", lastNr)
 }
 
-func printParsedInput(numbers map[int]int) {
-	for _, i := range numbers {
-		fmt.Printf("%d, ", i)
-	}
-}
-
-func parseInput(numbers string) (map[int]int, error) {
-	numMap := make(map[int]int)
+func parseInput(numbers string) (lastUsedIndex, int, int, error) {
+	lastUsed := make(lastUsedIndex)
 	strArray := strings.Split(numbers, ",")
-	for numIndex, numStr := range strArray {
-		num, err := strconv.Atoi(numStr)
+	var roundNr int
+	lastNr := 0
+	for roundNr = 1; roundNr <= len(strArray); roundNr++ {
+		num, err := strconv.Atoi(strArray[roundNr-1])
 		if err != nil {
-			return nil, err
+			return nil, 0, 0, err
 		}
-		numMap[numIndex] = num
-		fmt.Printf("%d, ", num)
+
+		lastUsed[num] = numberUsage{lastUsedRound: roundNr, firstTime: true, secondLastUsedDiff: 0}
+		lastNr = num
+		fmt.Printf("%d: Considering %d\n", roundNr, num)
 	}
-	return numMap, nil
+	return lastUsed, roundNr, lastNr, nil
 }
