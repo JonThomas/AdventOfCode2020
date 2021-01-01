2cc0b0f62ec90e144d081ca632185746e93eeb5b
Jon-Thomas <jonel@samfundet.no>
2021-01-01 17:17:16 +0100

    Finished 13b

--- src/Day13/Day13.go
+++ src/Day13/Day13.go
@@ -13,34 +13,66 @@ type busArrivaltime map[int]int
 
 func main() {
 
+	fmt.Println("Printing answer here, since it takes approx 4 hours to calulate: 760171380521445")
+
 	file, err := files.ReadFile("./Day13Input.txt")
 	if err != nil {
 		fmt.Println("File reading error", err)
 		return
 	}
 
-	departureTime, buses, parseErr := parseInput(file)
+	_, buses, parseErr := parseInput(file)
 	if parseErr != nil {
 		fmt.Println("parsing error", err)
 		return
 	}
 
-	printParsedInput(departureTime, buses)
+	printParsedInput(buses)
 
-	//minutesUntilArrival := make(busArrivaltime)
-	minutesUntilArrival := calulateArrivalTimes(departureTime, buses)
+	largestBusNumber, largestBusPosition := findLargestBusNr(buses)
 
-	// Shortest arrivaltime
-	shortestWait := 9999999999999
-	busNr := 0
-	for bus, minUntilArrival := range minutesUntilArrival {
-		if minUntilArrival < shortestWait {
-			shortestWait = minUntilArrival
-			busNr = bus
+	timestamp := 0
+	printSometimes := 0
+	// All buses departing at offsets matching their position in the list can only happen every (largestBusNumber) timestamp
+	// This is really only an optimization, to make the program run (largestBusNumber) times faster
+	for timestamp = largestBusNumber; true; timestamp += largestBusNumber {
+		if checkAllBuses(timestamp, largestBusPosition, buses) {
+			break // Found the solution
 		}
+		if printSometimes%10000000 == 0 {
+			fmt.Println(printSometimes)
+		}
+		printSometimes++
 	}
 
-	fmt.Printf("Answer: busId (%d) * shortestWait (%d) = %d", busNr, shortestWait, busNr*shortestWait)
+	fmt.Printf("Found solution at timestamp %d", timestamp-largestBusPosition)
+}
+
+func checkAllBuses(largestBusTimestamp int, largestBusPosition int, buses []int) bool {
+	// For each bus, check if its offset matches largestBusNumber
+	for busOffset := 0; busOffset < len(buses); busOffset++ {
+		thisBus := buses[busOffset]
+		if thisBus == -1 {
+			continue
+		}
+
+		if (largestBusTimestamp-largestBusPosition+busOffset)%thisBus != 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func findLargestBusNr(buses []int) (int, int) {
+	largestNr := 0
+	largestNrIndex := 0
+	for busIndex, bus := range buses {
+		if bus > largestNr {
+			largestNr = bus
+			largestNrIndex = busIndex
+		}
+	}
+	return largestNr, largestNrIndex
 }
 
 func calulateArrivalTimes(departureTime int, buses []int) busArrivaltime {
@@ -53,8 +85,7 @@ func calulateArrivalTimes(departureTime int, buses []int) busArrivaltime {
 	return minutesUntilArrival
 }
 
-func printParsedInput(timestamp int, buses []int) {
-	fmt.Println("Timestamp: ", timestamp)
+func printParsedInput(buses []int) {
 	fmt.Printf("Buses: ")
 	for _, busNr := range buses {
 		fmt.Printf("%d, ", busNr)
@@ -72,6 +103,7 @@ func parseInput(fileLines []string) (int, []int, error) {
 
 	for _, number := range strings.Split(fileLines[1], ",") {
 		if number == "x" {
+			buses = append(buses, -1)
 			continue
 		}
 
